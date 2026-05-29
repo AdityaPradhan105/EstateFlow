536367e8443156b6ecaeb7e1cf2c021d48629230
Aditya Pradhan <adityapradhan2124@gmail.com>
2026-05-29 06:40:22 +0530

    updates

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : import.meta.env.VITE_API_BASE_URL || "https://opms-project.onrender.com";
+    : import.meta.env.VITE_API_BASE_URL || "https://estateflow-343y.onrender.com/";
 
 export default API_BASE_URL;
